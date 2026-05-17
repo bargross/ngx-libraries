0a6b170899e6fcb5e0c85323c5256da0c0eb21ed
Leo Mengesha <barggros@gmail.com>
2026-05-17 22:34:43 +0100

    updated readme and removed redundant files

--- ngx-libraries-workspace/projects/ngx-update-notifier/src/lib/ngx-update-notifier.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ml-ngx-update-notifier',
-  imports: [],
-  template: ` <p>ngx-update-notifier works!</p> `,
-  styles: ``,
-})
-export class NgxUpdateNotifier {}
--- ngx-libraries-workspace/projects/ngx-update-notifier/src/lib/ngx-updater-notifier.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ml-ngx-updater-notifier',
-  imports: [],
-  template: ` <p>ngx-updater-notifier works!</p> `,
-  styles: ``,
-})
-export class NgxUpdaterNotifier {}
