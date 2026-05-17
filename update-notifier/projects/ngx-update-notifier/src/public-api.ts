9e0f60621207e682d2a394c43ec1eed38565fc9c
Leo Mengesha <barggros@gmail.com>
2026-05-17 19:56:46 +0100

    removed files

--- update-notifier/projects/ngx-update-notifier/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ng-update-notifier
- */
-
-export { VersionCheckService, VersionInfo } from './lib/services/ngx-update-notifier.service';
-export { UpdateNotifierComponent } from './lib/components/update-notifier/update-notifier';
-export { APP_VERSION } from './lib/tokens/update-notifier-token';
